b1fc268a481b23006bf1920814c211b7a144620b
TQ Hirsch <thequux@thequux.com>
2016-09-14 20:06:54 +0200

    Refactored player-specific rendering out of the main loop

--- player/qaraoke/src/main.rs
+++ player/qaraoke/src/main.rs
@@ -4,6 +4,7 @@ extern crate cdg_renderer;
 extern crate glium;
 extern crate image;
 extern crate fps_counter;
+use std::borrow::Cow;
 
 // TODO: Add glium_pib for bare metal Raspberry Pi support
 
@@ -16,15 +17,19 @@ struct Vertex {
 implement_vertex!(Vertex, position, tex_coords);
 
 fn main() {
-
+    let args: Vec<String> = std::env::args().collect();
+    let filename = args.get(1).expect("Usage: $0 filename");
+    let mut player = CdgPlayer::new(filename).unwrap();
     use glium::DisplayBuild;
 
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
     let billboard_vtx = [
-        Vertex{position: [-0.5, -0.5], tex_coords: [0.0, 0.0]},
-        Vertex{position: [-0.5,  0.5], tex_coords: [0.0, 1.0]},
-        Vertex{position: [ 0.5,  0.5], tex_coords: [1.0, 1.0]},
-        Vertex{position: [ 0.5, -0.5], tex_coords: [1.0, 0.0]},
+        // Note that the texture coordinates are inverted from GL coordinates
+        // you'd expect; this puts 0,0 at the top left corner
+        Vertex{position: [-0.5, -0.5], tex_coords: [0.0, 1.0]},
+        Vertex{position: [-0.5,  0.5], tex_coords: [0.0, 0.0]},
+        Vertex{position: [ 0.5,  0.5], tex_coords: [1.0, 0.0]},
+        Vertex{position: [ 0.5, -0.5], tex_coords: [1.0, 1.0]},
     ];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &billboard_vtx).unwrap();
@@ -57,29 +62,40 @@ fn main() {
 "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
+    
     use std::io::Cursor;
 
     let mut interp = cdg_renderer::CdgInterpreter::new();
     let mut frame_count = 0;
     let mut fps = fps_counter::FPSCounter::new();
+    let start_time = std::time::Instant::now();
+
     loop {
+        // Do updates
+        let playtime = std::time::Instant::now().duration_since(start_time);
+        player.update((playtime.as_secs() * 1000) as u32 + playtime.subsec_nanos() / 1000000);
+        let player_image = player.render();
+
+        // Render
         use glium::Surface;
-        use image::GenericImage;
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
 
-        let mut image = image::RgbaImage::new(300,216);
-        image.copy_from(&interp, 0,0);
-        
-        let glimage = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), (300,216));
+        let glimage = glium::texture::RawImage2d{
+            data: Cow::Borrowed(&player_image),
+            width: 300,
+            height: 216,
+            format: glium::texture::ClientFormat::U8U8U8U8,
+        };
+        //let glimage = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), (300,216));
         let texture = glium::texture::Texture2d::new(&display, glimage).unwrap();
         let uniforms = uniform!{
             tex: texture.sampled().magnify_filter(glium::uniforms::MagnifySamplerFilter::Nearest),
         };
         target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
-   
+
+        // Handle events
         for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return,
@@ -95,4 +111,53 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct CdgPlayer {
+    cdgStream: cdg::SubchannelStreamIter<Box<std::io::Read>>,
+    interp: cdg_renderer::CdgInterpreter,
 
+    current_sector: u32,
+    finished: bool,
+
+    out_buffer: image::RgbaImage,
+}
+
+
+impl CdgPlayer {
+    fn new(filename: &str) -> std::io::Result<Self> {
+        let file = Box::new(try!(std::fs::File::open(filename)));
+        
+        Ok(CdgPlayer{
+            cdgStream: cdg::SubchannelStreamIter::new(file),
+            interp: cdg_renderer::CdgInterpreter::new(),
+
+            current_sector: 0,
+            finished: false,
+
+            out_buffer: image::RgbaImage::new(300,216),
+        })
+    }
+
+    /// Update playback to time `time`, measured in milliseconds since
+    /// start of playback.
+    /// # Returns
+    /// 
+    fn update(&mut self, time: u32) {
+        let target_sector = time * 3 / 40;
+        while self.current_sector < target_sector && !self.finished {
+            if let Some(cmds) = self.cdgStream.next() {
+                for cmd in cmds {
+                    self.interp.handle_cmd(cmd)
+                }
+            } else {
+                self.finished = true;
+            }
+            self.current_sector += 1;
+        }
+    }
+
+    fn render(&mut self) -> &image::RgbaImage {
+        use image::GenericImage;
+        self.out_buffer.copy_from(&self.interp, 0,0);
+        &self.out_buffer
+    }
+}
